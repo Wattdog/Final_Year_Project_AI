33a3d1c04e049f2662cab4820b566b651897ea90
Aaron <aaronharrisonwatt1@gmail.com>
2022-02-25 11:25:44 +0000

    mlagents package added agent script started

---
+++ Final Year Project - AI/Assets/Scripts/PathFinderAgent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class PathFinderAgent : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+
+        }
+
+        if (collision.gameObject.CompareTag("No Trap"))
+        {
+
+        }
+    }
+}
